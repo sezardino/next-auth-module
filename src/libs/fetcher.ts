eeb98cd9969a83ac67ead926d9659de28653a5a4
Edward Arechwa <e.arechwa@nebucode.com>
2023-05-06 08:57:16 +0200

    ft: add interceptors for response

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -1,3 +1,4 @@
+import { apiService } from "@/services/api";
 import { tokenService } from "@/services/token";
 import axios, { AxiosResponse, CreateAxiosDefaults } from "axios";
 
@@ -22,3 +23,28 @@ fetcher.interceptors.request.use((config) => {
 
   return config;
 });
+
+fetcher.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const token = tokenService.get();
+
+    if (!token) return Promise.reject(error);
+
+    if (error.config._retry) return Promise.reject(error);
+    error.config._retry = true;
+
+    if (error.response.status !== 401) return Promise.reject(error);
+
+    try {
+      const response = await apiService.auth.refresh();
+
+      if (!response.data.access_token) return Promise.reject(error);
+
+      tokenService.save(response.data.access_token, "cookie");
+      return fetcher(error.config);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+);
